92f01e60efab24e290c3fb6156e56e6df85a49a1
amy <ctrlalternativedelete@gmail.com>
2023-05-18 19:52:53 +1000

    delete doubly cloned code in generate src_image

--- src/image_generator.rs
+++ src/image_generator.rs
@@ -6,17 +6,12 @@ pub fn generate_src_image(code: &str) -> DynamicImage {
     let ha = HighlightingAssets::new();
     let (ps, ts) = (ha.syntax_set, ha.theme_set);
 
-    // check if truncate code works and maybe remove clone if possible
-    let mut cloned_code = code.to_owned();
-    let truncate_index = code.match_indices('\n').nth(50).map(|e| e.0).unwrap_or(code.len());
-    cloned_code.truncate(truncate_index);
-
     // Change this later to first choose syntax by file extension
-    let syntax = ps.find_syntax_by_first_line(&cloned_code).unwrap_or(ps.find_syntax_by_token("rs").unwrap());
+    let syntax = ps.find_syntax_by_first_line(&code).unwrap_or(ps.find_syntax_by_token("rs").unwrap());
     let theme = &ts.themes["Dracula"];
 
     let mut h = HighlightLines::new(syntax, theme);
-    let highlight = LinesWithEndings::from(&cloned_code)
+    let highlight = LinesWithEndings::from(&code)
         .map(|line| h.highlight_line(line, &ps))
         .collect::<Result<Vec<_>, _>>()
         .unwrap();
